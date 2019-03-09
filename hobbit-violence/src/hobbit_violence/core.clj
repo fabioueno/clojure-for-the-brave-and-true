92529f4513b9bd17dde8f8b9c0225459c92c0a06
Fábio Ueno <fabio.t.ueno@gmail.com>
2019-03-09 18:08:52 -0300

    [Hobbit Violence] Implement Symmetrizer
    - Create matching part function
    - Create symmetrize body parts function

--- hobbit-violence/src/hobbit_violence/core.clj
+++ hobbit-violence/src/hobbit_violence/core.clj
@@ -21,6 +21,23 @@
                              {:name "left-achilles" :size 1}
                              {:name "left-foot" :size 2}])
 
+(defn matching-part
+  [part]
+  {:name (clojure.string/replace (:name part) #"^left-" "right-")
+   :size (:size part)})
+
+(defn symmetrize-body-parts
+  "Expects a seq of maps that have a :name and :size"
+  [asym-body-parts]
+  (loop [remaining-asym-parts asym-body-parts
+         final-body-parts []]
+    (if (empty? remaining-asym-parts)
+      final-body-parts
+      (let [[part & remaining] remaining-asym-parts]
+        (recur remaining
+               (into final-body-parts
+                     (set [part (matching-part part)])))))))
+
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
